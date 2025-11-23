cf8aeddaa7abed407986d8331ea2f3ee74b8148e
yeying-xingchen <yeyingxingchen@yeah.net>
2025-11-23 20:12:41 +0800

    feat: :sparkles: 增加了使用帮助

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 @app.post("/")
 def main(info: dict):
     if info["post_type"]=="message":
+        if info["raw_message"].startswith("/oseddl"):
+            return oseddl.on_command(info)
         if info["message_type"] == "group":
             if info["raw_message"].startswith("/github"):
                 return github.on_command(info)
-            if info["raw_message"].startswith("/oseddl"):
-                return oseddl.on_command(info)
     return {}
--- plugins/oseddl/__init__.py
+++ plugins/oseddl/__init__.py
@@ -3,7 +3,22 @@ import yaml
 
 def on_command(info: dict):
     sub_command = info["raw_message"].replace("/oseddl ","")
+    sub_command = info["raw_message"].replace("/oseddl","")
     commands = sub_command.split()
+    if sub_command == "":
+        message = """Oseddl 功能使用帮助
+/oseddl activites 查看活动列表
+/oseddl competitions 查看比赛列表
+/oseddl conferences 查看会议列表
+"""
+        return {"reply": message}
+    elif commands[0] == "help":
+        message = """Oseddl 功能使用帮助
+/oseddl activites 查看活动列表
+/oseddl competitions 查看比赛列表
+/oseddl conferences 查看会议列表
+"""
+        return {"reply": message}
     resp = requests.get(f"https://github.com/hust-open-atom-club/open-source-deadlines/raw/refs/heads/main/data/{commands[0]}.yml")
     resp_info = yaml.load(resp.text, Loader=yaml.CLoader)
     message=""
@@ -14,8 +29,10 @@ def on_command(info: dict):
             active_list.append(active_info)
         message = f"""标题：{active_list[a]["title"]}
 介绍：{active_list[a]["description"]}
-时间：{active_list[a]["events"][0]["timeline"][0]["deadline"]}
-链接：{active_list[a]["events"][0]["link"]}"""
+下一个事件：
+  名称：{active_list[a]["events"][0]["timeline"][0]["comment"]}
+  时间：{active_list[a]["events"][0]["timeline"][0]["deadline"]}
+  链接：{active_list[a]["events"][0]["link"]}"""
         return {"reply": message}
     except IndexError:
         id = 1
